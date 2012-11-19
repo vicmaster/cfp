fcb88805956fe38247d95802c39ed55fafa5126c
David Padilla <david@crowdint.com>
2012-11-18 18:18:09 -0600

    Proposal field validations

--- app/models/cfp/proposal.rb
+++ app/models/cfp/proposal.rb
@@ -1,5 +1,8 @@
 module Cfp
   class Proposal < ActiveRecord::Base
-     attr_accessible :title, :abstract, :tags, :level
+    attr_accessible :title, :abstract, :tags, :level
+
+    validates :title, :presence => true
+    validates :abstract, :presence => true
   end
 end
