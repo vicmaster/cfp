f09bfb4100ef763ae56074d6dfbca9f2e7e238a8
David Padilla <david@crowdint.com>
2012-12-11 08:37:09 -0600

    Separate the controller code that sets the locale

--- app/controllers/cfp/controller.rb
+++ app/controllers/cfp/controller.rb
@@ -1,11 +1,14 @@
 class Cfp::Controller < ::ApplicationController
-  before_filter :check_for_profile
+  before_filter :check_for_profile, :set_locale
   layout 'cfp/application'
 
   def check_for_profile
     if current_user
       redirect_to new_profile_path if current_user.should_create_profile?
-      I18n.locale = current_user.profile.locale if current_user.profile
     end
+  end
+
+  def set_locale
+    I18n.locale = current_user.profile.locale if current_user && current_user.profile
   end
 end
