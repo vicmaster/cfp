f1fac063421d853ab51750c6692816d1adcf4096
David Padilla <david@crowdint.com>
2013-10-31 14:24:45 -0600

    Add Devise token to dummy app

--- spec/dummy/config/initializers/devise.rb
+++ spec/dummy/config/initializers/devise.rb
@@ -125,7 +125,7 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   # config.timeout_in = 30.minutes
-  
+
   # If true, expires auth token on session timeout.
   # config.expire_auth_token_on_timeout = false
 
@@ -229,4 +229,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  config.secret_key = '523bd6d985ce15a1c50e28236d87a5cd0aba25cf248458895d80ffa88a000448d479f52e0213d2e5656d4e8ea7f2a3'
 end
