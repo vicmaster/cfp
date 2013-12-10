829e33607e4fc77bcd8ad3ec4d620c82109ba0e8
David Padilla <david@crowdint.com>
2013-12-10 10:01:32 -0600

    Load decorators on main app

--- lib/cfp/engine.rb
+++ lib/cfp/engine.rb
@@ -1,5 +1,12 @@
 module Cfp
   class Engine < ::Rails::Engine
     isolate_namespace Cfp
+
+    # Load decorators on main app
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
